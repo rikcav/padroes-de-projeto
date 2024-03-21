4f92e61c055f3791d76797d0edfc1c6c3baad2f9
rikcav <henriquearau000@gmail.com>
2024-03-21 09:38:09 -0300

    feat: teste de implementação do singleton como module

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,21 @@
-import { MyDatabaseClassic } from "./creational/singleton/db/MyDatabaseClassic";
+import { MyDatabaseModule } from "./creational/singleton/db/MyDatabaseModule";
 
-const myDatabaseClassic = MyDatabaseClassic.instance;
-myDatabaseClassic.add({ name: "Henrique", age: 22 });
-myDatabaseClassic.add({ name: "João", age: 21 });
-myDatabaseClassic.add({ name: "Maria", age: 23 });
-myDatabaseClassic.show();
-
-console.log("---------------------------------------");
-myDatabaseClassic.remove(1);
-myDatabaseClassic.show();
+const myDatabaseModule = MyDatabaseModule;
+myDatabaseModule.add({ name: "Henrique", age: 22 });
+myDatabaseModule.add({ name: "João", age: 21 });
+myDatabaseModule.add({ name: "Maria", age: 23 });
+myDatabaseModule.show();
 
 console.log("---------------------------------------");
-const myDatabaseClassic2 = MyDatabaseClassic.instance;
-myDatabaseClassic2.add({ name: "Roberta", age: 19 });
-myDatabaseClassic2.add({ name: "Jonas", age: 18 });
-myDatabaseClassic2.show();
+myDatabaseModule.remove(1);
+myDatabaseModule.show();
+
+console.log("---------------------------------------");
+const myDatabaseModule2 = MyDatabaseModule;
+myDatabaseModule2.add({ name: "Roberta", age: 19 });
+myDatabaseModule2.add({ name: "Jonas", age: 18 });
+myDatabaseModule2.show();
 
 console.log("---------------------------------------");
 console.log("myDatabaseClassic === myDatabaseClassic2");
-console.log(myDatabaseClassic === myDatabaseClassic2);
+console.log(myDatabaseModule === myDatabaseModule2);
