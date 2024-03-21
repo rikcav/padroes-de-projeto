9c993230840aeb9e4e41f6aab4fe766ca86389cd
rikcav <henriquearau000@gmail.com>
2024-03-21 09:26:40 -0300

    feat: comparando as duas instâncias de MyDatabaseClassic

--- src/index.ts
+++ src/index.ts
@@ -15,3 +15,7 @@ const myDatabaseClassic2 = MyDatabaseClassic.instance;
 myDatabaseClassic2.add({ name: "Roberta", age: 19 });
 myDatabaseClassic2.add({ name: "Jonas", age: 18 });
 myDatabaseClassic2.show();
+
+console.log("---------------------------------------");
+console.log("myDatabaseClassic === myDatabaseClassic2");
+console.log(myDatabaseClassic === myDatabaseClassic2);
