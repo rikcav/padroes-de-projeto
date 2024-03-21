73002c30b695e2c362783f80977eeb0fe5518838
rikcav <henriquearau000@gmail.com>
2024-03-21 09:22:15 -0300

    feat: teste da implementação clássica do singleton

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,17 @@
-console.log("Olá, Mundo!")
+import { MyDatabaseClassic } from "./creational/singleton/db/MyDatabaseClassic";
+
+const myDatabaseClassic = MyDatabaseClassic.instance;
+myDatabaseClassic.add({ name: "Henrique", age: 22 });
+myDatabaseClassic.add({ name: "João", age: 21 });
+myDatabaseClassic.add({ name: "Maria", age: 23 });
+myDatabaseClassic.show();
+
+console.log("---------------------------------------");
+myDatabaseClassic.remove(1);
+myDatabaseClassic.show();
+
+console.log("---------------------------------------");
+const myDatabaseClassic2 = MyDatabaseClassic.instance;
+myDatabaseClassic2.add({ name: "Roberta", age: 19 });
+myDatabaseClassic2.add({ name: "Jonas", age: 18 });
+myDatabaseClassic2.show();
